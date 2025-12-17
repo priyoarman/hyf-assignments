8e1dd2cfe611a4ecfbda49084ca6aa5090aeee24
Arman Hossain <priyoarman1996@gmail.com>
2025-12-17 15:39:01 +0100

    Add class exercises for Intro to Backend

--- courses/foundation/intro-to-backend/exercise1.js
+++ courses/foundation/intro-to-backend/exercise1.js
@@ -3,12 +3,17 @@ import express from "express";
 const app = express();
 const port = 3000;
 
+const dateToday = new Date();
+const yearNow = dateToday.getFullYear();
+const monthNow = dateToday.getUTCMonth();
+
 app.get("/", (req, res) => {
   res.send("Hello from exercise 1!");
 });
 
 app.get("/currentYear", (req, res) => {
   // TODO: Implement this function to return a JSON object containing the current year
+  res.send(`It's not ${monthNow} ${yearNow} now`)
 });
 
 app.listen(port, () => {
--- courses/foundation/intro-to-backend/exercise2.js
+++ courses/foundation/intro-to-backend/exercise2.js
@@ -1,6 +1,8 @@
 import express from "express";
 import knex from "knex";
 
+const dbFile = "./database.sqlite3";
+
 const app = express();
 const port = 3000;
 
@@ -8,13 +10,16 @@ const port = 3000;
 const knexInstance = knex({
   client: "sqlite3",
   connection: {
-    filename: "./database.sqlite3",
+    filename: dbFile,
   },
-  useNullAsDefault: true,  // Omit warning in console
+  useNullAsDefault: true, // Omit warning in console
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello from exercise 2!");
+  res.send(`
+      <h1>Main Page</h1>
+      <p>Hello from Exercise 2!</p>
+  `);
 });
 
 // Here is an example of the first route, /all-users, which returns all users sorted by their ID
@@ -22,9 +27,33 @@ app.get("/all-users", async (req, res) => {
   const rows = await knexInstance.raw("SELECT * FROM users ORDER BY id ASC;");
   res.json(rows);
 });
-
 // TODO implement more routes here
 
+app.get("/only-names", async (req, res) => {
+  const nameRows = await knexInstance.raw(
+    "SELECT first_name, last_name FROM users ORDER BY id ASC;"
+  );
+  res.json(nameRows);
+});
+
+app.get("/only-emails", async (req, res) => {
+  const emailRows = await knexInstance.raw(
+    "SELECT email FROM users ORDER BY email ASC;"
+  );
+  res.json(emailRows);
+});
+
+app.post("/status", async (request, response) => {
+  const {id, name} = request.body;
+
+  if (!id || !name || name.length === 0) return response.sendStatus(400);
+
+  await knex.raw(`insert into status(id, name) values(${id}, "${name}")`);
+  response.sendStatus(200);
+});
+
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+  console.log(
+    `Server is listening. Go to http://localhost:${port} on browser or Postman`
+  );
 });
