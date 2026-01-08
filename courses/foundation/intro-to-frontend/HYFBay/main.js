058aab8cef06005c1dc33c01fda18108e62f24bb
Arman Hossain <priyoarman1996@gmail.com>
2026-01-08 12:19:37 +0100

    Add Intro to Frontend assignments

--- courses/foundation/intro-to-frontend/HYFBay/main.js
+++ courses/foundation/intro-to-frontend/HYFBay/main.js
@@ -6,6 +6,25 @@ console.log(products);
 // This should create the ul and the li's with the individual products details
 function renderProducts(products) {
   // your code goes here
+  const productList = document.querySelector("#product-list");
+  
+  productList.innerHTML = "";
+
+  products.forEach((product) => {
+    const listItem = document.createElement("li");
+    const title = document.createElement("h2");
+    title.textContent = product.name;
+    const price = document.createElement("div");
+    price.textContent = `Price: ${product.price} DKK`;
+    const rating = document.createElement("div");
+    rating.textContent = `Rating: ${product.rating}/10`;
+
+    listItem.appendChild(title);
+    listItem.appendChild(price);
+    listItem.appendChild(rating);
+
+    productList.appendChild(listItem);
+  });
 }
 
 renderProducts(products); 
