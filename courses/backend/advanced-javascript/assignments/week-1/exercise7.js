47c30a0a30d7546695848e84607f1f1cef3702ca
Arman Hossain <priyoarman1996@gmail.com>
2026-03-09 21:13:26 +0100

    add assignment exercise-7

---
+++ courses/backend/advanced-javascript/assignments/week-1/exercise7.js
@@ -0,0 +1,13 @@
+import { teas } from "../../data/teas.js";
+
+const countByType = teas.reduce((counts, tea) => {
+  const type = tea.type;
+
+  counts[type] = (counts[type] || 0) + 1;
+
+  return counts;
+}, {});
+
+console.log(countByType);
+// Expected: { green: 6, black: 6, herbal: 4, oolong: 2, white: 2 }
+
