e330b40d4621cebc83941cd419b4ca472ff61fde
Arman Hossain <priyoarman1996@gmail.com>
2026-04-30 10:00:06 +0200

    feat: add node.js week1 assignments: part A

---
+++ courses/backend/node/nodejs-week1/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import snippetRouter from "./api/src/routers/snippets.js";
+import tagRouter from "./api/src/routers/tags.js";
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Support parsing JSON requests
+app.use(express.json());
+
+app.use("/api/snippets", snippetRouter);
+app.use("/api/tags", tagRouter);
+
+app.get("/", (req, res) => {
+  res.send("This is a search engine");
+});
+
+app.listen(port, () => {
+  console.log(`Listening for requests on port ${port}`);
+});
